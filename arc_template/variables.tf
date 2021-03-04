41ab49f8f9229b6ef5c9af6631704fd2a1cecab4
ravishankar-r <61870829+ravishankar-r@users.noreply.github.com>
2021-03-04 15:44:15 +0530

    Create variables.tf

---
+++ arc_template/variables.tf
@@ -0,0 +1,45 @@
+# Declare TF variables
+variable "aws_region" {
+  default = "ap-south-1"
+}
+variable "aws_availabilityzone" {
+  default = "ap-south-1a"
+}
+
+variable "admin_username" {
+  default = "arcadmin"
+}
+
+variable "admin_password" {
+  default = "arcdemo123!!"
+}
+
+variable "azure_location" {
+  default = "westus2"
+}
+
+variable "hostname" {
+  default = "arc-aws-demo"
+}
+
+variable "azure_resource_group" {
+  default = "Arc-AWS-Demo"
+}
+
+variable "subscription_id" {
+}
+
+variable "client_id" {
+}
+
+variable "client_secret" {
+}
+
+variable "tenant_id" {
+}
+
+variable "access_key" {
+}
+
+variable "secret_key" {
+}
